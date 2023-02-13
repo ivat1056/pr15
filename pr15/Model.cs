d1cc4a547e2f4290e4db46571b2af390b29987a3
ivan matveev <you@gmail.com>
2023-02-13 20:22:08 +0300

    Добавьте файлы проекта.

---
+++ pr15/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace pr15
+{
+    internal class Model
+    {
+        public static string first;
+        public static string second;
+        public static string result = "";
+        public static int indexComboBox = -1;
+        public static List<string> dataList = new List<string> { "Сложение", "Вычитание", "Умножение", "Деление" };
+
+
+
+        public static TextBlock tbFirst;
+        public static TextBox tbResult;
+    }
+}
